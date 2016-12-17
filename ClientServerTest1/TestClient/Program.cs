91cb560b7d8c49b24764e8bfc8d1979bf9e1aa13
dominic hall <dominic hall>
2016-12-17 05:02:19 +0000

    Begun listening for clients and managing them

--- ClientServerTest1/TestClient/Program.cs
+++ ClientServerTest1/TestClient/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace TestClient
 {
@@ -10,6 +12,15 @@ namespace TestClient
     {
         static void Main(string[] args)
         {
+            TcpListener ConnectionListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 25566);
+            ConnectionListener.Start();
+            while (true)
+            {
+                while (!ConnectionListener.Pending())
+                {
+                    ConnectionListener.AcceptTcpClient();
+                }
+            }
         }
     }
 }

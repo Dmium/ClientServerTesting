eeb3408efa2d82e94c745335aebcd717322a3885
dominic hall <dominic hall>
2016-12-17 05:24:11 +0000

    Got bored. Added a client listener thread before leaving

--- ClientServerTest1/Server/ClientThreadManager.cs
+++ ClientServerTest1/Server/ClientThreadManager.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Server
 {
     class ClientThreadManager
     {
+        TcpClient Client;
+        Thread Listener;
+        NetworkStream Stream;
+        public ClientThreadManager(TcpClient Client)
+        {
+            this.Client = Client;
+            Listener = new Thread(new ThreadStart(ClientListener));
+            Listener.Start();
+        }
+        public void ClientListener()
+        {
+            try
+            {
+                Stream = Client.GetStream();
+                while (Client.Connected)
+                {
 
+                }
+            }
+            catch { }
+        }
     }
 }

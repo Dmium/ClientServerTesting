9669fa431c761af53492711cefa707ca773a1c52
Dom <dominiczy.hall@gmail.com>
2016-12-18 18:52:48 +0000

    Enough work has been done on Packets library to begin adapting Client and Server manager

---
+++ ClientServerTest1/Packets/DecodedPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Packets
+{
+    public class DecodedPacket
+    {
+        public int ClientID { get; }
+        public object obj { get; }
+        public DecodedPacket(int ClientID, object obj)
+        {
+            this.ClientID = ClientID;
+            this.obj = obj;
+        }
+    }
+}
--- ClientServerTest1/Packets/PacketMethods.cs
+++ ClientServerTest1/Packets/PacketMethods.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +18,25 @@ namespace Packets
             for (int i = 0; i < tempData.Length; i++)
                 tempData[i] = data[i + 4];
             return new NumberByteArray(number, tempData);
+        }
+        //public static byte[] ShortenPacket(byte[] data, int length)
+        //{
+        //    byte[] tempData = new byte[length];
+        //    for (int i = 0; i < tempData.Length; i++)
+        //        tempData[i] = data[i];
+        //    return tempData;
+        //}
+        public static object PacketToObject(NetworkStream Stream)
+        {
+            byte[] dataBuffer = new byte[4194304];
+            Stream.Read(dataBuffer, 0, dataBuffer.Length);
+            return (new BinaryFormatter()).Deserialize(new System.IO.MemoryStream(GetFirstInt(dataBuffer).ByteArray));
+        }
+        public static byte[] ObjectToByteArray(Object obj)
+        {
+            MemoryStream temp = new MemoryStream();
+            (new BinaryFormatter()).Serialize(temp, obj);
+            return temp.ToArray();
         }
     }
 }

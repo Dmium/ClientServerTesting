abf612524c9e4d3a0870cd0499fe214330e81197
Dom <dominiczy.hall@gmail.com>
2016-12-18 15:44:43 +0000

    Started work on packet library

---
+++ ClientServerTest1/Packets/NumberByteArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packets
+{
+    public class NumberByteArray
+    {
+        public int Number  { get; }
+        public byte[] ByteArray { get; }
+        public NumberByteArray(int Number, byte[] ByteArray)
+        {
+            this.Number = Number;
+            this.ByteArray = ByteArray;
+        }
+    }
+}
---
+++ ClientServerTest1/Packets/PacketMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packets
+{
+    public static class PacketMethods
+    {
+        public static NumberByteArray GetFirstInt(byte[] data)
+        {
+            int number = BitConverter.ToInt32(data, 0);
+            byte[] tempData = new byte[data.Length - 4];
+            for (int i = 0; i < tempData.Length; i++)
+                tempData[i] = data[i + 4];
+            return new NumberByteArray(number, tempData);
+        }
+    }
+}

32ec87fe119484ef8b57e629eb46122a43218380
Dom <dominiczy.hall@gmail.com>
2016-12-19 00:18:05 +0000

    Code for receiving objects now (hopefully) complete. just got to add sending and have the Thread manager be shared by the client and server.

--- ClientServerTest1/Server/Server.cs
+++ ClientServerTest1/Server/Server.cs
@@ -13,9 +13,11 @@ namespace ServerManager
 {
     public class Server
     {
-        const string ip = "127.0.0.1";
+        const string ip = "127.0.0.1";//Should probably have a constructor for this
         const int port = 25566;
         Thread ServerThread;
+        //Should probably make this a dictionary or something so the server require
+        //a restart every 2,147,483,647 connections (and maybe give my ram a break)
         private List<ClientThreadManager> Managers = new List<ClientThreadManager>();
         private ConcurrentQueue<DecodedPacket> ReceivedPackets = new ConcurrentQueue<DecodedPacket>();
         public bool CanRead { get { return !ReceivedPackets.IsEmpty; } }
@@ -37,7 +39,7 @@ namespace ServerManager
                 }
                 for (int i = 0; i < Managers.Count; i++)
                 {
-                    while(!Managers[i].Packets.IsEmpty)
+                    while (!Managers[i].Packets.IsEmpty)
                     {
                         //Yeah this is bad. If TryDequeue consistantly fails then the entire server crashes :/
                         if (Managers[i].Packets.TryDequeue(out cPacket))
@@ -46,5 +48,19 @@ namespace ServerManager
                 }
             }
         }
+        /// <summary>
+        /// Gets the next preloaded packet. Will return false if it fails (and dp will be null)
+        /// </summary>
+        /// <param name="dp"></param>
+        /// <returns></returns>
+        public bool GetPacket(out DecodedPacket dp)
+        {
+            if (CanRead)
+                if (ReceivedPackets.TryDequeue(out dp))
+                    return true;
+            dp = null;
+            return false;
+        }
+        //TODO add send methods (broadcast as well as client specific)
     }
 }
